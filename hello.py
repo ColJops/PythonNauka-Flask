2cf843de7b61cf1eaeac091d142a7821413e2ecd
ColJops <jops24@o2.pl>
2023-03-22 15:34:52 +0100

    dodanie obsługi bazy danych

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from datetime import datetime
 import os
@@ -13,11 +14,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
 app.config['SECRET_KEY'] = '###fdf32D34fg!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class NameForm(FlaskForm):
     name = StringField('Jak masz na imię?', validators=[DataRequired()])
@@ -27,7 +30,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' %self.name
@@ -45,15 +48,25 @@ class User(db.Model):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
+        #old_name = session.get('name')
+        user = User.query.filter_by(username=form.name.data).first()
+        #if old_name is not None and old_name != form.name.data:
+        if user is None:
+            """
             flash('Wygląda na to, że teraz nazywasz się inaczej ;)')
             return redirect(url_for('index'))
+            """
+            user = User(form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['know'] = False
+        else:
+            session['know'] = True
         name = form.name.data
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('user', name=name))
-    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'), know=session.get('know', False))
 
 @app.route('/user/<name>')
 def user(name):
@@ -67,6 +80,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
 
 if __name__ == '__main__':
     app.run(
