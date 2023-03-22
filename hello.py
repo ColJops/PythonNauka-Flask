4db0906faf56765df67f87463d9566bac69f4e94
ColJops <jops24@o2.pl>
2023-03-22 11:18:21 +0100

    Wprowadzono formularz i sesję

--- hello.py
+++ hello.py
@@ -1,16 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, session, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = '###fdf32D34fg!'
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('Jak masz na imię?', validators=[DataRequired()])
+    submit = SubmitField('Wyślij')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Wygląda na to, że teraz nazywasz się inaczej ;)')
+            return redirect(url_for('index'))
+        name = form.name.data
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('user', name=name))
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
